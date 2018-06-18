a7e77b2758758d432db6fc3630f1dc436921cf6a
Alexander Halemba <alexander.halemba@hotelbird.com>
2018-06-18 16:22:36 +0200

    fix Server info change PW

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,3 @@
 import { ApiServer } from './api-server';
 
-process.env.DB_Type = process.env.DB_Type || 'postgres';
-process.env.DB_Host = process.env.DB_Host || '195.201.113.80';
-process.env.DB_Port = process.env.DB_Port || '5432';
-process.env.DB_Username = process.env.DB_Username || 'apiTestserver';
-process.env.DB_Password = process.env.DB_Password || 'Y7s4YYCBQFNTJnQWrcKH4ty8';
-process.env.DB_Database = process.env.DB_Database || 'apiTestdatabase';
-process.env.Port = process.env.Port || '8111';
-
 export const server = new ApiServer();
